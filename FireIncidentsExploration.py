ad24447f4f9386b44aa2fb3f9c7146c2fc3c8d5e
Gaurav Kumar <kumar.gauravnitk@gmail.com>
2017-11-04 08:54:42 +0100

    Update FireIncidentsExploration.py

--- FireIncidentsExploration.py
+++ FireIncidentsExploration.py
@@ -10,10 +10,6 @@
 #The entry point into all functionality in Spark 2.0 is the new SparkSession class:
 
 ```
->>> content.cache()
-```
-
-```
 spark
 <pyspark.sql.session.SparkSession object at 0x0000000005C0FC88>
 ```
