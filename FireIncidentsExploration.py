3443e4b21082e1c9557b939c15fdafd47346dceb
Gaurav Kumar <kumar.gauravnitk@gmail.com>
2017-11-04 08:51:28 +0100

    Exploring the City of San Francisco public data with Apache Spark 2.0
    Exploring the City of San Francisco public data with Apache Spark 2.0

---
+++ FireIncidentsExploration.py
@@ -0,0 +1,13 @@
+# Exploring the City of San Francisco public data with Apache Spark 2.0
+# There are **2 APIs** for interacting with Spark:
+# **DataFrames/SQL/Datasets:** general, higher level API for users of Spark
+# **RDD:** a lower level API for spark internals and advanced programming
+# Introduction to Fire Department Calls for Service
+# Note, you can also access the 1.6 GB of data directly from sfgov.org via this link: https://data.sfgov.org/Public-Safety/Fire-Department-Calls-for-Service/nuek-vuh3
+
+# Below commands are in iterative manner which I have used using **Pyspark** session
+
+#The entry point into all functionality in Spark 2.0 is the new SparkSession class:
+```spark
+<pyspark.sql.session.SparkSession object at 0x0000000005C0FC88>
+```
